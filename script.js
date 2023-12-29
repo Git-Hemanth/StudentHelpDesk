8a106c36e38350b2a35cd22e334e57c7e4152cd5
Hemanth Bhargav Dasari <147172782+Git-Hemanth@users.noreply.github.com>
2023-12-29 21:35:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,139 @@
-function showPage(pageId) {
-    hideAllPages();
-    document.getElementById(pageId).style.display = 'block';
-  }
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  showHome();  // Show home content by default
+});
+document.addEventListener('DOMContentLoaded', function () {
+  showHome();  // Show home content by default
+});
+
+
+function showHome() {
+  hideAllSections();
+  document.getElementById('homeContent').style.display = 'block';
+}
+
+function showQueryForm() {
+  hideAllSections();
+  document.getElementById('queryForm').style.display = 'block';
+}
+
+function toggleFAQ() {
+  var faqSection = document.getElementById('faqSection');
+  var homeContent = document.getElementById('homeContent');
+
+  hideAllSections();
+  faqSection.style.display = 'block';
+  homeContent.style.display = 'none';
+}
+
+function toggleAnswer(card) {
+  var answer = card.querySelector('.answer');
+  answer.style.display = (answer.style.display === 'none' || answer.style.display === '') ? 'block' : 'none';
+}
+
+function submitQuery() {
+  var submitButton = document.getElementById('submitQuerybtn');
   
-  function submitQuery() {
-    // Implement your query submission logic here
-    // For this example, let's just show a success message
-  
-    const responseDiv = document.createElement('div');
-    responseDiv.innerHTML = '<p>Query submitted successfully!</p>';
-    responseDiv.style.color = '#4CAF50'; // Green color
-  
-    const queryForm = document.getElementById('query-form');
-    queryForm.parentNode.replaceChild(responseDiv, queryForm);
-  }
-  
-  function hideAllPages() {
-    const pages = document.querySelectorAll('.page');
-    pages.forEach(page => {
-      page.style.display = 'none';
-    });
-  }
-  
-  
+  // Disable the button to prevent multiple clicks
+  submitButton.disabled = true;
+
+  // Your form submission logic goes here
+  var successMessage = document.getElementById('successMessage');
+  successMessage.style.display = 'block';
+
+  // Optional: You can redirect after a delay
+  setTimeout(function () {
+    window.location.href = 'index.html';
+  }, 2000);
+
+  // Enable the button back after the action is complete
+  setTimeout(function () {
+    submitButton.disabled = false;
+  }, 2000);
+
+  // Return false to prevent the form from submitting (remove this line if not needed)
+  return false;
+}
+
+function hideAllSections() {
+  var sections = ['homeContent', 'queryForm', 'faqSection'];
+  sections.forEach(function(sectionId) {
+    document.getElementById(sectionId).style.display = 'none';
+  });
+}
+// script.js
+
+// script.js
+// script.js
+
+// script.js
+
+// script.js
+
+// ... (your existing code) ...
+
+// script.js
+
+
+function openContactModal() {
+  var contactModal = document.getElementById('contactModal');
+  contactModal.style.display = 'block';
+}
+
+function closeContactModal() {
+  var contactModal = document.getElementById('contactModal');
+  contactModal.style.display = 'none';
+}
+
+function submitContactForm() {
+  // You can add logic to handle the submission of the contact form
+  alert('Contact form submitted!'); // Replace with your actual logic
+
+  // Close the modal after submission
+  closeContactModal();
+
+  // Prevent the default form submission
+  return false;
+}
+
+function openContactModal() {
+  var contactModal = document.getElementById('contactModal');
+  contactModal.style.display = 'block';
+}
+
+function closeContactModal() {
+  var contactModal = document.getElementById('contactModal');
+  contactModal.style.display = 'none';
+}
+
+function submitContactForm() {
+  // You can add logic to handle the submission of the contact form
+  alert('Contact form')}
+
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  showHome();  // Show home content by default
+});
+
+function openContactModal() {
+  var contactModal = document.getElementById('contactModal');
+  contactModal.style.display = 'block';
+}
+
+function closeContactModal() {
+  var contactModal = document.getElementById('contactModal');
+  contactModal.style.display = 'none';
+}
+
+function submitContactForm() {
+  // You can add logic to handle the submission of the contact form
+  alert('Contact form submitted!'); // Replace with your actual logic
+
+  // Close the modal after submission
+  closeContactModal();
+
+  // Prevent the default form submission
+  return false;
+}
